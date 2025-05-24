263578167b1d5a76e4020ef65da61e84f51ab700
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-23 23:49:15 -0700

    Prompter example

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String name = Prompter.promptSimple("User Name");
+        int age = Prompter.promptIntSimple("Age");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String color = Prompter.prompt("Color", "Black");
+        int width = (int) Prompter.promptInt("Width", 100);
+
+        double feet = Prompter.promptDouble("Feet", 5.0);
     }
 }
